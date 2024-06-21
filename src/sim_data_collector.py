f04c60258b744d25e95c9386db24017e960978fb
ofe.dagan <ofer.dagan@colorado.edu>
2024-06-21 11:35:16 -0600

    Updated simulation data collector

--- src/sim_data_collector.py
+++ src/sim_data_collector.py
@@ -2,6 +2,7 @@
 
 import rospy
 import numpy as np
+import csv
 
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Pose
@@ -10,17 +11,23 @@ from nav_msgs.msg import Path
 from std_msgs.msg import Bool
 from multi_slam_and_tracking_ros.msg import Results
 
-TEST_NAME = "sim_test"
+TEST_NAME = "data/sim2"
 
 class DataCollector:
     def __init__(self):
         # Create arrays
         self.tars_kimera = []
+        # self.tars_kimera.append(["x","y","z"])
         self.tars_truth = []
+        # self.tars_truth.append(["x","y","z"])
         self.kipp_LIOSAM = []
+        # self.kipp_LIOSAM.append(["x","y","z"])
         self.kipp_truth = []
+        # self.kipp_truth.append(["x","y","z"])
         self.target_truth = []
+        # self.target_truth.append(["x","y","z"])
         self.results = []
+        # self.results.append(["TimeStep","Agent","Target","LambdaMin","TMu","TMuDim","TCov","TCovDim"])
 
         self.tars_single_truth = None
         self.kipp_single_truth = None
@@ -66,7 +73,14 @@ class DataCollector:
                                 self.kipp_single_truth.position.z])
         
     def results_callback(self, msg):
-        self.results.append(msg)
+        self.results.append([str(msg.TimeStep),
+                             str(msg.Agent),
+                             str(msg.Target),
+                             str(msg.TMu),
+                             str(msg.TMuDim),
+                             str(msg.TCov),
+                             str(msg.TCovDim)])
+        # self.results.append(msg)
 
         if self.current_time_step != msg.TimeStep:
             self.current_time_step = msg.TimeStep
@@ -77,18 +91,30 @@ class DataCollector:
         
     def save_callback(self, msg):
         print("saving data")
-        with open(TEST_NAME + "_tars_truth.csv", "wb") as f:
-            np.savetxt(f, self.tars_truth, delimiter =", ", fmt ='% s')
-        with open(TEST_NAME + "_kipp_truth.csv", "wb") as f:
-            np.savetxt(f, self.kipp_truth, delimiter =", ", fmt ='% s')
-        with open(TEST_NAME + "_target_truth.csv", "wb") as f:
-            np.savetxt(f, self.target_truth, delimiter =", ", fmt ='% s')
-        with open(TEST_NAME + "_tars_kimera.csv", "wb") as f:
-            np.savetxt(f, self.tars_kimera, delimiter =", ", fmt ='% s')
-        with open(TEST_NAME + "_kipp_LIOSAM.csv", "wb") as f:
-            np.savetxt(f, self.kipp_LIOSAM, delimiter =", ", fmt ='% s')
-        with open(TEST_NAME + "_results.csv", "wb") as f:
-            np.savetxt(f, self.results, delimiter =", ", fmt ='% s')
+        with open(TEST_NAME + "_tars_truth.csv", "w") as f:
+            write = csv.writer(f)
+            write.writerow(['x','y','z'])
+            write.writerows(self.tars_truth)
+        with open(TEST_NAME + "_kipp_truth.csv", "w") as f:
+            write = csv.writer(f)
+            write.writerow(['x','y','z'])
+            write.writerows(self.kipp_truth)
+        with open(TEST_NAME + "_target_truth.csv", "w") as f:
+            write = csv.writer(f)
+            write.writerow(['x','y','z'])
+            write.writerows(self.target_truth)
+        with open(TEST_NAME + "_tars_kimera.csv", "w") as f:
+            write = csv.writer(f)
+            write.writerow(['x','y','z'])
+            write.writerows(self.tars_kimera)
+        with open(TEST_NAME + "_kipp_LIOSAM.csv", "w") as f:
+            write = csv.writer(f)
+            write.writerow(['x','y','z'])
+            write.writerows(self.kipp_LIOSAM)
+        with open(TEST_NAME + "_results.csv", "w") as f:
+            write = csv.writer(f)
+            write.writerow(['TimeStep','Agent','Target','TMu','TMuDim','TCov','TCovDim'])
+            write.writerows(self.results)
 
         print('--------data saved---------')
         
